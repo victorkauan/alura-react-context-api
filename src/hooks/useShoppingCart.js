760d8d7630a2c3a98cc0afc58281360203d0830b
Victor Kauan <victor.kauan@moldsoft.email>
2025-05-04 18:01:01 -0300

    :sparkles: feat: Create use shopping cart context hook

---
+++ src/hooks/useShoppingCart.js
@@ -0,0 +1,47 @@
+import { useContext } from "react"
+import { ShoppingCartContext } from "@/contexts/ShoppingCartContext"
+
+export const useShoppingCartContext = () => {
+  const { shoppingCart, setShoppingCart } = useContext(ShoppingCartContext)
+
+  function getUpdatedShoppingCart(id, quantity) {
+    return shoppingCart.map(product => {
+      if (product.id === id) {
+        product.quantidade += quantity
+      }
+
+      return product
+    })
+  }
+
+  function addProduct(newProduct) {
+    const hasProduct = shoppingCart.some((product) => product.id === newProduct.id)
+
+    if (!hasProduct) {
+      newProduct.quantidade = 1
+      return setShoppingCart(previous => [...previous, newProduct])
+    }
+
+    const updatedShoppingCart = getUpdatedShoppingCart(newProduct.id, 1)
+    setShoppingCart([...updatedShoppingCart])
+  }
+
+  function removeProduct(id) {
+    const productToRemove = shoppingCart.find(product => product.id === id)
+    const isLastUnit = productToRemove.quantidade === 1
+
+    if (isLastUnit) {
+      return setShoppingCart(previous => previous.filter(product => product.id !== id))
+    }
+
+    const updatedShoppingCart = getUpdatedShoppingCart(id, -1)
+    setShoppingCart([...updatedShoppingCart])
+  }
+
+  return {
+    shoppingCart,
+    setShoppingCart,
+    addProduct,
+    removeProduct
+  }
+}
